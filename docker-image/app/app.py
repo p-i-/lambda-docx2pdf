92de66b0b0b6a89cedb28b4c4c6d886e2c40e6a7
π <pi@pipad.org>
2021-01-07 16:19:35 +0700

    Tidied app.py

--- docker-image/app/app.py
+++ docker-image/app/app.py
@@ -1,7 +1,7 @@
-import sys
 import json
 
+import platform
+print(platform.python_version())
 
 def handler(event, context): 
-    # TODO "Event data:" + event.dumps()
-    return 'Hello from AWS Lambda using Python' + sys.version + '!' + 'Event data:' + json.dumps(event)
+    return f'Hello from AWS Lambda using Python {platform.python_version()}, Event data: {json.dumps(event)}'
